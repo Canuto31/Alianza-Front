7601d38d467ff535d7f56ab35e81194111b1ce04
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-09-01 19:07:33 -0500

    Creation of the service

--- src/app/client/service/client.service.ts
+++ src/app/client/service/client.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../client.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+  private baseUrl = 'http://localhost:8090/alianza/api/cliente';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getClients(): Observable<Client[]> {
+    const url = `${this.baseUrl}`;
+    return this.http.get<Client[]>(url);
+  }
 }
